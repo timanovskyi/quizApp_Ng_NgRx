ddbaf1c989cc8672ccd283483bc82ea494646826
egor <test@test.com>
2022-03-15 13:32:00 +0100

    added styles

---
+++ quizApp_Ng_NgRx/angular-quiz/src/app/quiz/data.ts
@@ -0,0 +1,23 @@
+const data = [
+  {
+    question: 'What does CSS stand for?',
+    incorrectAnswers: [
+      'Computer Style Sheets',
+      'Creative Style Sheets',
+      'Colorful Style Sheets'
+    ],
+    correctAnswer: 'Cascading Style Sheets'
+  },
+  {
+    question: 'Where is an HTML document is the correct place to refer to an external style sheet?',
+    incorrectAnswers: [
+      'I the body section',
+      'At the end of the document',
+      'You can\'t refer to an external style sheet',
+    ],
+    correctAnswer: 'In the head section'
+  }
+]
+
+
+export default data
